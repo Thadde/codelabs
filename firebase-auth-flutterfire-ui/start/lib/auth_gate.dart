a653850025c1903a7e8d137be2715cd87099e12e
Thadde <81052526+Thadde@users.noreply.github.com>
2024-03-18 02:31:25 +0100

    Update auth_gate.dart

--- firebase-auth-flutterfire-ui/start/lib/auth_gate.dart
+++ firebase-auth-flutterfire-ui/start/lib/auth_gate.dart
@@ -1,11 +1,14 @@
+// ignore_for_file: use_super_parameters
+
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'home.dart';
 
+
 class AuthGate extends StatelessWidget {
- const AuthGate({super.key});
+ const AuthGate({Key? key}) : super(key: key);
 
  @override
  Widget build(BuildContext context) {
@@ -26,32 +29,17 @@ class AuthGate extends StatelessWidget {
                ),
              );
            },
-           subtitleBuilder: (context, action) {
-             return Padding(
-               padding: const EdgeInsets.symmetric(vertical: 8.0),
-               child: action == AuthAction.signIn
-                   ? const Text('Welcome to FlutterFire, please sign in!')
-                   : const Text('Welcome to Flutterfire, please sign up!'),
-             );
-           },
+           
            footerBuilder: (context, action) {
              return const Padding(
                padding: EdgeInsets.only(top: 16),
                child: Text(
-                 'By signing in, you agree to our terms and conditions.',
-                 style: TextStyle(color: Colors.grey),
+                 'En vous coonectant,vous acceptez les conditions utilisateurs.',
+                 style: TextStyle(color: Color.fromARGB(255, 77, 64, 64)),
                ),
              );
            },
-           sideBuilder: (context, shrinkOffset) {
-             return Padding(
-               padding: const EdgeInsets.all(20),
-               child: AspectRatio(
-                 aspectRatio: 1,
-                 child: Image.asset('flutterfire_300x.png'),
-               ),
-             );
-           },
+           
          );
        }
        return const HomeScreen();
